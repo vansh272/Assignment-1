e818dcfded93aca2150234b5e8595e16097946f7
vansh272 <shahvansh272@gmail.com>
2025-08-12 22:21:29 +0530

    Create add multiply divide subtract divide.c

---
+++ add multiply divide subtract divide.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter number 1: ");
+    scanf("%d", &a);
+    printf("Enter number 2: ");
+    scanf("%d", &b);
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Product: %d\n", a * b);
+    if (b != 0)nswer
+        printf("Answer of division  %d\n", a / b);
+    else
+        printf("Cannot divide by zero\n");
+    return 0;
+}
