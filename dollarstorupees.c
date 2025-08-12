ec43528d11dc94d7f742c7ea4eb10104a7991350
vansh272 <shahvansh272@gmail.com>
2025-08-12 22:26:32 +0530

    Create dollarstorupees.c

---
+++ dollarstorupees.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float dollar;
+    printf("Enter amount in dollars: ");
+    scanf("%f", &dollar);
+    printf("Rupees: %f\n", dollar * 87.59);
+    return 0;
+}
