beaf7ced3e4768c0cfe78dcc5db24310accc7c57
vansh272 <shahvansh272@gmail.com>
2025-08-12 22:17:19 +0530

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter number 1: ");
+    scanf("%d", &a);
+    printf("Enter number 2: ");
+    scanf("%d", &b);
+    printf("Product: %d\n", a * b);
+    return 0;
+}
