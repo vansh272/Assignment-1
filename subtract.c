5c2b6396c04d003505e073b90cf94c39f61fda1f
vansh272 <shahvansh272@gmail.com>
2025-08-12 22:15:46 +0530

    Create subtract.c

---
+++ subtract.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter number 1: ");
+    scanf("%d", &a);
+    printf("Enter number 2: ");
+    scanf("%d", &b);
+    printf("Difference: %d\n", a - b);
+    return 0;
+}
